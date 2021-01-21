a938ee898670686794feba833a3e89197cca8527
Fabrizio Gortani <fabrizio.gortani@protonmail.com>
2021-01-21 21:46:37 +0000

    Update hssh.rb

--- Formula/hssh.rb
+++ Formula/hssh.rb
@@ -1,8 +1,8 @@
 class Hssh < Formula
   desc "An utility to easily search through your ssh configs and connect into remote servers. Also you can use a gitlab repo to store your config files and sync it!"
   homepage "https://github.com/CasvalDOT/hssh"
-  url "https://github.com/CasvalDOT/hssh/archive/v2.0.0.tar.gz"
-  sha256 "2cf074b033f3343a57e589a8c16341d1f40d061c76fed13660a55acf86db2288"
+  url "https://github.com/CasvalDOT/hssh/archive/v1.0.0.tar.gz"
+  sha256 "35dbc12fd071598b3b7a85a6e14c759c0d15959c50aa1a56b93ec2f0becf1de7"
   license "MIT"
 
   depends_on "go" => :build
